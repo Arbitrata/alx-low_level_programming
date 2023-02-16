9e21e62f780d52ae2a3a0db45af439004601e26d
arbitrata <andrewmbugua76@gmail.com>
2023-02-16 06:31:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-int main (void) {
+
+/**
+ * main is the entry point
+ * 0 is to be returned
+ */
+
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
 		return 0;
 }
