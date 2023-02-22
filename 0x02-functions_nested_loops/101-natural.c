88868af329145fbe81cfcb77e83ce64c838996f2
arbitrata <andrewmbugua76@gmail.com>
2023-02-22 01:30:26 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,10 +7,11 @@
  * Return: Always  0 (Success)
  *
  */
-int main () 
+int main(void) 
 {
 	int sum = 0;
 	int i;
+
 	for(i = 1; i < 1024; i++)
 	{
 		if((i % 3) == 0 || (i % 5) == 0)
@@ -18,6 +19,8 @@ int main ()
 			sum += i;
 		}
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
