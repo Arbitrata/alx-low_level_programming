d3929f529701abc9086b382ec3306cb91c6f686a
arbitrata <andrewmbugua76@gmail.com>
2023-02-23 01:09:11 -0800

    0-task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - Test function for negative-positive
+ * main - tests whether the number is positive or negative
  *
- * Return: Always 0
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }	
